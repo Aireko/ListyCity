6a2d89d9137e66ae45e06101d99fb5d1020af20e
Phil <jly2360@outlook.com>
2026-02-11 14:52:20 -0700

    Revert "generate javadoc"
    This reverts commit 39b017b1a6c69dd74feec9f10ef22dc5b3fade52.

--- app/src/main/java/com/example/listycity/CityList.java
+++ app/src/main/java/com/example/listycity/CityList.java
@@ -1,7 +1,9 @@
 package com.example.listycity;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+
 /**
  * This is a class that keeps track of a list of city objects
  */
--- app/src/main/java/com/example/listycity/MainActivity.java
+++ app/src/main/java/com/example/listycity/MainActivity.java
@@ -8,7 +8,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
