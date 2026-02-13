28517c004862bb5a1b0a05d95f0b9b584aa91424
Phil <jly2360@outlook.com>
2026-02-13 10:53:34 -0700

    feat: implemented hasCity, delete and countCities

--- app/src/main/java/com/example/listycity/CityList.java
+++ app/src/main/java/com/example/listycity/CityList.java
@@ -30,4 +30,19 @@ public class CityList {
         Collections.sort(list);
         return list;
     }
+
+    public boolean hasCity(City city) {
+        return cities.contains(city);
+    }
+
+    public void delete(City city) {
+        if (!cities.contains(city)) {
+            throw new IllegalArgumentException("City not found in list");
+        }
+        cities.remove(city);
+    }
+
+    public int countCities() {
+        return cities.size();
+    }
 }
